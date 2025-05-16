5e6f00dbc0d057de13ff8b2c901da09a355b27ff
manuelgarfia0 <manuel.garfia@iesnervion.es>
2025-05-16 09:13:09 +0200

    Primer archivo

---
+++ BD.js
@@ -0,0 +1,23 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.7.3/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.7.3/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyBjzgHVxAyqkXG1Nn5Qw1dQcTSJwJgP6mg",
+    authDomain: "primerproyecto-3bb22.firebaseapp.com",
+    projectId: "primerproyecto-3bb22",
+    storageBucket: "primerproyecto-3bb22.firebasestorage.app",
+    messagingSenderId: "42355729259",
+    appId: "1:42355729259:web:a8a045f4be9a657ad8e22e",
+    measurementId: "G-EQBLXQLERY"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
